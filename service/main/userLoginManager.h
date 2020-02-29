8794fcc54900a2335d0432d75b43d6b2b46d1b56
xiaohui.tan <xiaohui.tan@yingzt.com>
2020-02-29 20:54:55 +0800

    test service build

--- service/main/userLoginManager.h
+++ service/main/userLoginManager.h
@@ -6,7 +6,7 @@
 #include<string.h>
 #include<unistd.h>
 #include <grpcpp/grpcpp.h>
-#include "logrecord.h"
+#include "mylib/mylibLog/logrecord.h"
 #include "protos/proto/messageReceiver.grpc.pb.h"
 
 using namespace std;
